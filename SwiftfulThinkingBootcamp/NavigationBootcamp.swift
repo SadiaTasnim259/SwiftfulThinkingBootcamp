ab24470f50fe56e2c1890ec7127521d1630540bb
Sadia Tasnim <tasnimsadia259@gmail.com>
2024-06-04 13:47:17 +0600

    complete navigation view

--- SwiftfulThinkingBootcamp/NavigationBootcamp.swift
+++ SwiftfulThinkingBootcamp/NavigationBootcamp.swift
@@ -13,13 +13,36 @@ struct NavigationBootcamp: View {
             ScrollView {
                 NavigationLink("Go to other screen", destination: MyOtherScreen())
                 
-                Text("Hello,World")
-                Text("Hello,World")
-                Text("Hello,World")
-                Text("Hello,World")
+                ForEach(0..<50) { _ in
+                    Text("Hello,World")
+                }
+                
+              
+               
             }
             .navigationTitle("All Inboxes")
             .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    HStack {
+                        Image(systemName: "person.fill")
+                            .foregroundColor(.indigo)
+                        Image(systemName: "flame.fill")
+                            .foregroundColor(.red)
+                    }
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink(destination: FavoriteView()) {
+                        Image(systemName: "book.circle.fill")
+                            .foregroundColor(.green)
+                        Image(systemName: "heart.fill")
+                            .foregroundColor(.red)
+                    }
+                    
+                }
+                
+            }
+            //.navigationBarItems(leading: Image(systemName: "person.fill"), trailing: Image(systemName: "heart.fill"))
         }
     }
 }
@@ -30,9 +53,12 @@ struct MyOtherScreen: View{
    
             
                 ZStack {
-                    Color.indigo.edgesIgnoringSafeArea(.all)
+                    Color.indigo.ignoresSafeArea()
                     
-                    NavigationLink("Go to 3rd screen", destination: ThirdScreen())
+                    NavigationLink( destination: ThirdScreen()){
+                        Text("Go to 3rd screen")
+                            .foregroundColor(Color.white)
+                    }
                 }.navigationTitle("Other Screen")
                 //.navigationBarHidden(true)
             
@@ -41,15 +67,38 @@ struct MyOtherScreen: View{
 }
 
 struct ThirdScreen: View{
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View{
         ZStack {
-            Color.gray.edgesIgnoringSafeArea(.all)
+            Color.gray.ignoresSafeArea()
             
+            VStack {
+                Button("Go back") {
+                    dismiss()
+                }
+            }
         }
         .navigationTitle("3rd Screen")
+        .navigationBarBackButtonHidden(true)
     }
 }
 
+struct FavoriteView: View{
+    @Environment(\.dismiss) private var dismiss
+    var body: some View{
+        ZStack {
+            Color.black.ignoresSafeArea()
+            VStack {
+                Button("Go Back to Inbox") {
+                    dismiss()
+                }
+            }
+        }
+        .navigationTitle("Favorite View")
+        
+    }
+}
 
 struct NavigationBootcamp_Previews: PreviewProvider {
     static var previews: some View {
